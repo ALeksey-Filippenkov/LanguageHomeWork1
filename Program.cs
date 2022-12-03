242de5db51db82b3c47c8746bd941101488b9e01
Aleksey <fil_aleksey@hotmail.com>
2022-12-03 22:22:49 +0300

    some formatting

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
 // int number1 = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Введите два второе число");
 // int number2 = Convert.ToInt32(Console.ReadLine());
-// if (number1 > number2)
+// if (number1 >= number2)
 //     System.Console.WriteLine($"max = {number1}");
 // else
 //     System.Console.WriteLine($"max = {number2}");
